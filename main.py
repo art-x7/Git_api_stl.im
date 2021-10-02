9a1cede5638a774791e1dfe34ef790dccd19cf26
Kernel Kernel <artx8wot@gmail.com>
2021-10-02 19:28:32 +0200

    add logger

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
-from datetime import datetime, timedelta
 
+from log.logger import logger
+from datetime import datetime, timedelta
 from endpoints import GitHubEndpoints
 from flask import Flask
 from flask_restful import abort, Api, Resource
-
 
 app = Flask(__name__)
 api = Api(app)
@@ -18,7 +18,6 @@ pulls_url = url + GitHubEndpoints.pulls.value
 issues_url = url + GitHubEndpoints.issues.value
 forks_url = url + GitHubEndpoints.forks.value
 
-print(app.config.get("GIT_API_TOKEN"))
 
 @app.route("/")
 def hello():
@@ -36,43 +35,40 @@ def abort_if_keys_doenst_exist(key, data):
 
 
 class Git_id(Resource):
-    r = requests.get(url=url, headers=headers)
-    data = r.json()
-
     def get(self, key):
-        abort_if_keys_doenst_exist(key, self.data)
+        r = requests.get(url=url, headers=headers)
+        data = r.json()
+        abort_if_keys_doenst_exist(key, data)
         keys_list = []
-        if isinstance(self.data, list):
-            for item in self.data:
+        if isinstance(data, list):
+            for item in data:
                 keys_list.append(item[key])
             return keys_list
         else:
-            return self.data[key]
+            return data[key]
 
 
 class Git_list(Resource):
-    r = requests.get(url=url, headers=headers)
-    data = r.json()
-
     def get(self):
-        return self.data
+        r = requests.get(url=url, headers=headers)
+        data = r.json()
+        return data
 
 
 class Git_pulls(Resource):
-    r = requests.get(url=pulls_url, headers=headers)
-    data = r.json()
-
     def get(self):
-        return self.data
+        r = requests.get(url=pulls_url, headers=headers)
+        data = r.json()
+        logger.debug(data)
+        return data
 
 
 class Git_pulls_week(Resource):
-    r = requests.get(url=pulls_url, headers=headers)
-    data = r.json()
-
     def get(self):
+        r = requests.get(url=pulls_url, headers=headers)
+        data = r.json()
         pulls_above_two_weeks = []
-        for item in self.data:
+        for item in data:
             last_update = datetime.fromisoformat(item["updated_at"][:-1])
             two_weeks_date = datetime.now() - timedelta(weeks=2)
             if last_update < two_weeks_date:
@@ -82,19 +78,17 @@ class Git_pulls_week(Resource):
 
 
 class Git_issues(Resource):
-    r = requests.get(url=issues_url, headers=headers)
-    data = r.json()
-
     def get(self):
-        return self.data
+        r = requests.get(url=issues_url, headers=headers)
+        data = r.json()
+        return data
 
 
 class Git_forks(Resource):
-    r = requests.get(url=forks_url, headers=headers)
-    data = r.json()
-
     def get(self):
-        return self.data
+        r = requests.get(url=forks_url, headers=headers)
+        data = r.json()
+        return data
 
 
 api.add_resource(Git_id, '/api/<key>')
@@ -103,7 +97,6 @@ api.add_resource(Git_pulls, '/api/pulls')
 api.add_resource(Git_pulls_week, '/api/pulls_w')
 api.add_resource(Git_issues, '/api/issues')
 api.add_resource(Git_forks, '/api/forks')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
